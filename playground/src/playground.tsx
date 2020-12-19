54cc5f7d15df1dc218f1fa7fad37dee53eba9904
Manuel Vila <mvila@3base.com>
2020-12-19 09:35:25 +0900

    Update TS config

--- playground/src/playground.tsx
+++ playground/src/playground.tsx
@@ -427,26 +427,30 @@ export function Playground() {
       <h3 css={sectionStyle}>Table</h3>
 
       <table>
-        <tr>
-          <th>1</th>
-          <th>2</th>
-          <th>3</th>
-        </tr>
-        <tr>
-          <td>1-1</td>
-          <td>1-2</td>
-          <td>1-3</td>
-        </tr>
-        <tr>
-          <td>2-1</td>
-          <td>2-2</td>
-          <td>2-3</td>
-        </tr>
-        <tr>
-          <td>3-1</td>
-          <td>3-2</td>
-          <td>3-3</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1-1</td>
+            <td>1-2</td>
+            <td>1-3</td>
+          </tr>
+          <tr>
+            <td>2-1</td>
+            <td>2-2</td>
+            <td>2-3</td>
+          </tr>
+          <tr>
+            <td>3-1</td>
+            <td>3-2</td>
+            <td>3-3</td>
+          </tr>
+        </tbody>
       </table>
 
       <hr />
